db6515a2e44147be6e0127216992ba0e35d09d7c
Kristina Dortman <101277777+EnovaSan@users.noreply.github.com>
2022-09-07 23:17:05 +0300

    Add files via upload

---
+++ Construct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace кря
+{
+    public class Construct
+    {
+        public static void infoDuck(string type, string move ) 
+        {
+            Console.WriteLine("Утка " + type +" " + move);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace кря
+{
+    class Program : Construct
+    {
+        public static void Main(string[] args)
+        {
+
+            List<string> Ducks = new List<string>{"живая", "плюшевая", "пластмассовая" };
+
+            foreach(var duck in Ducks)
+            {
+                if (duck == "живая")
+                {
+                    infoDuck(duck, "кря");
+                }
+                else
+                {
+                    infoDuck(duck, "лежит, не двигается");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
