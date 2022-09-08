da2f9c51ccf089a8c9b8a0a93e96260a5b187382
Kristina Dortman <101277777+EnovaSan@users.noreply.github.com>
2022-09-09 01:02:00 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,26 +3,87 @@ using System.Collections.Generic;
 
 namespace кря
 {
-    class Program : Construct
+    abstract class DuckSound
     {
-        public static void Main(string[] args)
+        public abstract void Activity();
+
+        public void DuckInfo()
         {
+            Console.WriteLine(GetType().Name);
+        }
 
-            List<string> Ducks = new List<string>{"живая", "плюшевая", "пластмассовая" };
+    }
 
-            foreach(var duck in Ducks)
+    class ScotlandDuck : DuckSound
+    {
+        public override void Activity()
+        {
+            Console.WriteLine("квак-квак\n");
+        }
+    }
+
+    class ItalianDuck : DuckSound
+    {
+        public override void Activity()
+        {
+            Console.WriteLine("ква-ква\n");
+        }
+    }
+
+    class DanishDuck : DuckSound
+    {
+        public override void Activity()
+        {
+            Console.WriteLine("рэп-рэп\n");
+        }
+    }
+
+    class RussianDuck : DuckSound
+    {
+        public override void Activity()
+        {
+            Console.WriteLine("кря-кря\n");
+        }
+    }
+
+    class UndeadDuck : DuckSound
+    {
+        public override void Activity()
+        {
+            Console.WriteLine("тоби конец!");
+        }
+    }
+
+    class Sound
+    {
+        public void Activity(DuckSound duckSound)
+        {
+            duckSound.Activity();
+        }
+
+        public void DuckInfo(DuckSound duckSound)
+        {
+            duckSound.DuckInfo();
+        }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sound sound = new Sound();
+
+            DuckSound[] ducks = { new ScotlandDuck(), new ItalianDuck(), new RussianDuck(), new DanishDuck(), new UndeadDuck() };
+
+            foreach (var item in ducks)
             {
-                if (duck == "живая")
-                {
-                    infoDuck(duck, "кря");
-                }
-                else
-                {
-                    infoDuck(duck, "лежит, не двигается");
-                }
+                sound.DuckInfo(item);
+                sound.Activity(item);
             }
-
-            Console.ReadLine();
+           
         }
     }
 }
+ 
+      
